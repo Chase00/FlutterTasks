2d99b86093bf92f2b1b60d4a6a3658e1e623886c
Chase00 <chasekuhnmc@gmail.com>
2020-05-06 23:16:02 -0400

    Adding tasks, completing tasks, and the completed task history page

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() => runApp(MaterialApp(
   theme: ThemeData(
     brightness: Brightness.dark,
     primaryColor: Colors.deepPurple,
-    accentColor: Colors.blueAccent,
+    accentColor: Colors.deepPurple,
   ),
   home: MyApp(),
 ));
@@ -17,60 +17,104 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   List tasks = List();
   List saved = List();
+  String input = "";
 
   @override
   void initState() {
-    tasks.add("Task");
-    tasks.add("Task1");
-    tasks.add("Task2");
-    tasks.add("Task3");
+    tasks.add("Cook Dinner");
+    tasks.add("Fold laundry");
+    tasks.add("Finish unfinished projects");
+    tasks.add("Do jumping jacks");
+    tasks.add("Complete Flutter solo project");
   }
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Task List"),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.star), onPressed: _pushSaved),
+          IconButton(icon: Icon(Icons.star), onPressed: _pushStarred),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                shape: RoundedRectangleBorder(borderRadius:
+                BorderRadius.circular(8)),
+                title: Text("Add Task"),
+                content: TextField(
+                  onChanged: (String value) {
+                    input = value;
+                  },
+                ),
+                actions: <Widget>[
+                  FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        tasks.add(input);
+                      });
+                      Navigator.of(context).pop();
+                    },
+                  child: Text("Add"))
+                ],
+              );
+            }
+          );
+        },
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
       body: ListView.builder(
-          itemCount: tasks.length,
-          itemBuilder: (BuildContext content, int index) {
-            return Card(
-                elevation: 20,
-                child: ListTile(
-                  leading: Icon(
-                    Icons.star_border,
-                    size: 30,
+        itemCount: tasks.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Dismissible(
+            key: Key(tasks[index]),
+            child: Card(
+              elevation: 3,
+              margin: EdgeInsets.all(6),
+              shape: RoundedRectangleBorder(borderRadius:
+              BorderRadius.circular(8)),
+              child: ListTile(
+                title: Text(tasks[index]),
+                  leading: IconButton(
+                    icon: Icon(Icons.event_note),
                     color: Colors.white30,
                   ),
-                  trailing: Icon(
-                    Icons.delete,
-                    size: 30,
-                    color: Colors.grey,
-                  ),
-                  title: Text(tasks[index]),
-//                  onTap: () {
-//                    Navigator.push(context, MaterialPageRoute(
-//                        builder: (context) => ClassInfo(classInfo: course.getCode(index),)));
-//                  },
-                )
-            );
-          }),
+                trailing: IconButton(
+                  icon: Icon(Icons.star_border),
+                  color: Colors.deepPurple[300],
+                  iconSize: 30,
+                  onPressed: () {
+                    setState(() {
+                      saved.add(tasks[index]);
+                      tasks.removeAt(index);
+                    });
+                  },
+                ),
+              )
+            )
+          );
+        },
+      )
     );
   }
-  void _pushSaved() {
+  void _pushStarred() {
     Navigator.of(context).push(
       MaterialPageRoute<void>(
         builder: (BuildContext context) {
           final Iterable<ListTile> tiles = saved.map(
-                (tasks) {
+                (text) {
               return ListTile(
-                title: Text(
-                  tasks,
+                leading: IconButton(
+                  icon: Icon(Icons.check),
+                  color: Colors.green,
                 ),
+                title: Text(text),
               );
             },
           );
@@ -81,12 +125,11 @@ class _MyAppState extends State<MyApp> {
           ).toList();
           return Scaffold(
             appBar: AppBar(
-              title: Text('Priority Tasks'),
+              title: Text('Completed Tasks'),
             ),
             body: ListView(children: divided),
           );
         },
       ),
     );
-  }
-}
+}}
